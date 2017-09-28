eae9a9a15a03aada4b8c394b56c70ce643bbe7e7
Elena Savelyeva <elena.savelyeva@touchin.ru>
2017-09-28 21:44:57 +0300

    Added writeup
    nothing functional, just a skeleton to further discussion

---
+++ bookswapper.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Simple Bot to reply to Telegram messages
+# This program is dedicated to the public domain under the CC0 license.
+"""
+This Bot uses the Updater class to handle the bot.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+book_list = ['Book1','Book2'] #TODO persistance
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    update.message.reply_text('Привет! С помощью этого чата можно организовывать обмен книгами в вашем чате')
+
+
+def help(bot, update):
+    update.message.reply_text('Добавить книгу поможно с помощью команды /addbook, просмотреть список доступных с помощью команды /list')
+
+
+def addbook(bot, update, args):
+	book_list.append(''.join(args))
+
+def booklist(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text='\n'.join(book_list))
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater("TOKEN")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("addbook", addbook,  pass_args=True)) # TODO implement conversation handlers
+    dp.add_handler(CommandHandler("list", booklist)) 
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
